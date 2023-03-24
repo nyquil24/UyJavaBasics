1f1ba54ef39a39e3a3060f2dd38f12801a8cf069
Opie Taylor <taborn.nequil@gmail.com>
2023-03-23 20:43:05 -0500

    practice

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int myVariable;
+        myVariable = 5;
+
+        System.out.println(myVariable);
+
+
+    }
+}
