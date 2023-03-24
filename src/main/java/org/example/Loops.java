24f63b9152af024761356e4e392ed772ce369224
Opie Taylor <taborn.nequil@gmail.com>
2023-03-23 21:58:39 -0500

    loops

---
+++ src/main/java/org/example/Loops.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class Loops {
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+
+    static void whileLoop(){
+        int counter = 0;
+        while(counter < 10){
+            counter = counter + 1;
+            System.out.println(counter);
+        }
+
+    }
+
+    static void doWhileLoop(){
+        int counter = 10;
+        do{
+            counter = counter + 1;
+            System.out.println(counter);
+        } while(counter < 10);
+    }
+
+    static void forLoop(){
+        for(int counter=0; counter < 10; counter = counter + 1){
+            System.out.println(counter);
+        }
+    }
+
+}
