1f55329f1db25ceda06b4addd480225d6623a6e4
Opie Taylor <taborn.nequil@gmail.com>
2023-03-24 10:39:19 -0500

    access modifiers

--- src/main/java/org/example/Objects.java
+++ src/main/java/org/example/Objects.java
@@ -6,14 +6,29 @@ public class Objects {
     public static void main(String[] args) {
 
 
-        int number = 5;
-
-        JFrame window = new JFrame();
-
-        window.setTitle("My Window");
-        window.setSize(800,600);
-        window.setVisible(true);
 
 
     }
+    //public visibility means that this mehtod can be accessed anywhere in the program so
+    //long as you have an instance of this class to call it from
+    public void doSomethingPublic(){
+
+    }
+    //private visibility means that this method can't be accessed anywhere other than inside of this class
+    private void doSomethingPrivate(){
+
+    }
+    // protected visibility means that this method can only be accessed inside of this package,
+    // and from subclasses of this class
+    protected void doSomethingProtected(){
+
+    }
+    // default visibility mean that this method can only be accessed inside of this package
+    void doSomething(){
+
+    }
+
+
+
+
 }
