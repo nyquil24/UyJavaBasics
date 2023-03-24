6333177f8d67eb4ece064dfe3607b2cef860366d
Opie Taylor <taborn.nequil@gmail.com>
2023-03-23 21:22:15 -0500

    conditional

---
+++ src/main/java/org/example/Conditionals.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Conditionals {
+    public static void main(String[] args) {
+
+        boolean sayHello = true;
+        boolean sayGoodbye = false;
+
+        if(sayHello) {
+            System.out.println("Hello");
+        }else{
+            System.out.println("Goodbye");
+        }
+        }
+
+}
