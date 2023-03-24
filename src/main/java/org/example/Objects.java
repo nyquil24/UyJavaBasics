2cbdb27c82030baafa62a39186c6865a83b0057a
Opie Taylor <taborn.nequil@gmail.com>
2023-03-23 22:31:23 -0500

    feature/objects

---
+++ src/main/java/org/example/Objects.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Objects {
+    public static void main(String[] args) {
+
+
+        int number = 5;
+
+        JFrame window = new JFrame();
+
+        window.setTitle("My Window");
+        window.setSize(800,600);
+        window.setVisible(true);
+
+
+    }
+}
